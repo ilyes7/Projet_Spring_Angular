2a1a70d78457af05e6a1e30bef01aff728ed4cb6
haribizohra <zohra.haribi@esprit.tn>
2021-11-03 08:18:06 +0100

    IfactureService et IdetailfactureService CRUD Repo

--- src/main/java/tn/esprit/spring/Service/IDetailFactureService.java
+++ src/main/java/tn/esprit/spring/Service/IDetailFactureService.java
@@ -1,5 +1,23 @@
 package tn.esprit.spring.Service;
 
+import java.text.ParseException;
+import java.util.List;
+import java.util.Optional;
+
+import tn.esprit.spring.entity.DetailFacture;
+
+
 public interface IDetailFactureService {
+	
+	List<DetailFacture> retrieveAllDetailFacture();
+
+	DetailFacture addDetailFacture(DetailFacture c) throws ParseException;
+
+	void deleteDetailFacture(Long id);
+
+	DetailFacture updateDetailFacture(DetailFacture c);
+
+	Optional<DetailFacture> retrieveDetailFacture(Long id);
+
 
 }
--- src/main/java/tn/esprit/spring/Service/IFactureService.java
+++ src/main/java/tn/esprit/spring/Service/IFactureService.java
@@ -1,5 +1,21 @@
 package tn.esprit.spring.Service;
 
+import java.text.ParseException;
+import java.util.List;
+import java.util.Optional;
+
+import tn.esprit.spring.entity.Facture;
+
 public interface IFactureService {
+	List<Facture> retrieveAllFacture();
+
+	Facture addFacture(Facture c) throws ParseException;
+
+	void deleteFacture(Long id);
+
+	Facture updateFacture(Facture c);
+
+	Optional<Facture> retrieveFacture(Long id);
+
 
 }
