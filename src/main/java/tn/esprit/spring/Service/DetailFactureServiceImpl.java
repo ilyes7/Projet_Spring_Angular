67c1e45215a91c2bb0d7f71aec9dc752f065a291
haribizohra <zohra.haribi@esprit.tn>
2021-11-03 08:21:17 +0100

    entete et import implementation

--- src/main/java/tn/esprit/spring/Service/DetailFactureServiceImpl.java
+++ src/main/java/tn/esprit/spring/Service/DetailFactureServiceImpl.java
@@ -1,5 +1,49 @@
 package tn.esprit.spring.Service;
 
-public class DetailFactureServiceImpl {
+import java.text.ParseException;
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.stereotype.Service;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import tn.esprit.spring.entity.DetailFacture;
+
+
+@RestController
+@RequestMapping(value = "/detail-facture")
+@Service
+public class DetailFactureServiceImpl implements IDetailFactureService {
+
+	@Override
+	public List<DetailFacture> retrieveAllDetailFacture() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public DetailFacture addDetailFacture(DetailFacture c) throws ParseException {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public void deleteDetailFacture(Long id) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public DetailFacture updateDetailFacture(DetailFacture c) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Optional<DetailFacture> retrieveDetailFacture(Long id) {
+		// TODO Auto-generated method stub
+		return null;
+	}
 
 }
--- src/main/java/tn/esprit/spring/Service/FactureServiceImpl.java
+++ src/main/java/tn/esprit/spring/Service/FactureServiceImpl.java
@@ -1,5 +1,49 @@
 package tn.esprit.spring.Service;
 
-public class FactureServiceImpl {
+import java.text.ParseException;
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.stereotype.Service;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import tn.esprit.spring.entity.Facture;
+
+
+@RestController
+@RequestMapping(value = "/facture")
+@Service
+public class FactureServiceImpl implements IFactureService {
+
+	@Override
+	public List<Facture> retrieveAllFacture() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Facture addFacture(Facture c) throws ParseException {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public void deleteFacture(Long id) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public Facture updateFacture(Facture c) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Optional<Facture> retrieveFacture(Long id) {
+		// TODO Auto-generated method stub
+		return null;
+	}
 
 }
